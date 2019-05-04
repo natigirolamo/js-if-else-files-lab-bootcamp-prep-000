9bf6dffe9927ffe2bbb0f53cda156a733c1cf93c
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-04 17:47:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,9 +5,3 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
-
-if (name === "Natalia") {
-  console.log("The name is correct")
-} else {
-  console.log("This is wrong, expected name to be Natalia")
-}
